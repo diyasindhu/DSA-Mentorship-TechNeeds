13733fa1bbeb616f284a2e2e6fb6bfdacc41451a
Diya Sindhu <146509534+diyasindhu@users.noreply.github.com>
2024-01-05 03:07:36 +0530

    Create a2_q20.cpp

---
+++ a2_q20.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of rows ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j==1 || j==n){
+                cout<<"*\t";
+            }
+            else if ((i>n/2) && (j==i || i+j==n+1)){
+                cout<<"*\t";
+            }
+            else{
+                cout<<"\t";
+            }
+
+            }
+            cout<<endl;
+        }
+    }
+   
